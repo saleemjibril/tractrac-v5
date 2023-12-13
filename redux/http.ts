ad8e9fd769301c8dc1a2b1b014971ada8620de3c
IntrovertDev <tobiemman1@gmail.com>
2023-12-13 11:22:46 +0100

    Updated API base url

--- redux/http.ts
+++ redux/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 // import { baseUrl } from './utils/helpers'
 
 const http = axios.create({
-  baseURL: "https://api.tractrac.co/v1",
+  baseURL: "https://tractrac.iiimpact.org/v1",
   headers: {
     'Content-Type': 'application/json',
   },
